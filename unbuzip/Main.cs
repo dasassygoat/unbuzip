87e05498726f311e77a926e5a27b7da0b0d8a940
Bryan Pritchett <bryan@golemshead.com>
2013-12-31 02:14:03 -0500

    Initial commit, fixed directory problem now need to fix message that the folders are empty

---
+++ unbuzip/Main.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+
+using System.Text;
+using Ionic.Zip;
+
+
+namespace Unzipper
+{
+    class Program
+    {
+        
+        
+
+        static void Main(string[] args)
+        {
+            string dirpath = "/media/bryan/New Volume/";
+            string extractDirectory = "/media/bryan/New Volume/temp/";
+            DirectoryInfo di = new DirectoryInfo(dirpath);
+
+            foreach (FileInfo fi in di.GetFiles("*.zip"))
+            {
+                //add to database
+                Decompress(fi, extractDirectory);
+				//Console.WriteLine("found file");
+            }
+            Console.WriteLine("Finished decompressing");
+            Console.ReadKey();
+
+            
+        }
+
+        public static void Decompress (FileInfo fi, string extractDirectory)
+		{
+			//UnzipDataDataContext db = new UnzipDataDataContext();
+			string compressedFile = fi.DirectoryName + "/" + fi.Name;
+			string fileName = fi.Name.Replace (".zip", "");
+			string directory = fi.DirectoryName + "/" + fileName;
+
+
+			//ZipFileName zipname = new ZipFileName();
+
+			
+            
+			/*var found = from name in db.ZipFileNames
+                        where name.ZipFileName1 == fileName & name.Extracted == true
+                        select name;
+            zipname.ZipFileName1 = fileName;
+            if (found.Count() == 0)
+            { */
+
+			try {
+				Directory.CreateDirectory (directory);
+				Console.WriteLine("Created Directory at {0}\n\n", directory);
+				//ZipFile.ExtractToDirectory(compressedFile, directory);
+
+				using (ZipFile zip = ZipFile.Read(compressedFile))
+                {
+                    // This call to ExtractAll() assumes:
+                    //   - none of the entries are password-protected.
+                    //   - want to extract all entries to current working directory
+                    //   - none of the files in the zip already exist in the directory;
+                    //     if they do, the method will throw.
+                    zip.ExtractAll(directory);
+                }
+
+				//zipname.Extracted = true;
+				//File.Delete(compressedFile);
+
+                    Console.WriteLine ("Decompressed: {0}", fi.Name);
+
+			} catch (Exception e) {
+				//zipname.Extracted = false;
+				Console.WriteLine ("Couldn't decompress: {0} Error: {1}\n\n", fi.Name, e.ToString ());
+			}
+		}
+
+
+	
+
+    }
+}
+
